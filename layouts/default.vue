5ddda49efccc53709ae1bd7685ea16f40354a379
lebLuiz <leb.e.b@hotmail.com>
2024-02-08 22:51:43 -0300

    feat: useFetch e useAsyncData

--- layouts/default.vue
+++ layouts/default.vue
@@ -60,6 +60,12 @@
                     >
                         Vídeos Favoritos
                     </NuxtLink>
+                    <NuxtLink
+                        to="/clientes"
+                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                    >
+                        Clientes
+                    </NuxtLink>
                 </div>
                 <div>
                     <ClientOnly>
